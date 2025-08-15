362d816269483a2e73239c29183ce4c27694fff1
huseyinbaglayici <huseyinow35@hotmail.com>
2025-08-15 20:05:36 +0300

    Initialized Camera System

---
+++ Assets/Scripts/Runtime/Enums/CameraTargetStates.cs
@@ -0,0 +1,8 @@
+﻿namespace Runtime.Enums
+{
+    public enum CameraTargetStates
+    {
+        Player,
+        FakePlayer
+    }
+}
---
+++ Assets/Scripts/Runtime/Managers/CameraManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Cinemachine;
+using Runtime.Enums;
+using Runtime.Signals;
+using Sirenix.OdinInspector;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class CameraManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region SerializeField Variables
+
+        [SerializeField] private CinemachineStateDrivenCamera stateDrivenCamera;
+        [SerializeField] private Animator animator;
+
+        #endregion
+
+        #region Private Variables
+
+        [ShowInInspector] private float3 _initalPosition;
+
+        #endregion
+
+        #endregion
+
+        #region Event Subscriptions
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _initalPosition = transform.position;
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CoreGameSignals.Instance.onReset += OnReset;
+            CameraSignals.Instance.onSetCameraTarget += OnSetCinemachineTarget;
+            CameraSignals.Instance.onChangeCameraState += OnChangeCameraState;
+        }
+
+        private void OnSetCinemachineTarget(CameraTargetStates state)
+        {
+            switch (state)
+            {
+                case CameraTargetStates.Player:
+                {
+                    // var playerManager = FindObjectOfType<PlayerManager>.transform;
+                    // stateDrivenCamera.follow = playerManager;
+                }
+                    break;
+                case CameraTargetStates.FakePlayer:
+                {
+                    stateDrivenCamera.Follow = null;
+                    // var fakePlayer = FindObjectOfType<WallCheckController>().transform.parent.transform;
+                }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+        }
+
+        private void OnChangeCameraState(CameraStates state)
+        {
+            animator.SetTrigger(state.ToString());
+        }
+
+
+        private void UnSubscribeEvents()
+        {
+            CoreGameSignals.Instance.onReset -= OnReset;
+            CameraSignals.Instance.onSetCameraTarget -= OnSetCinemachineTarget;
+            CameraSignals.Instance.onChangeCameraState -= OnChangeCameraState;
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+
+        private void OnReset()
+        {
+            CameraSignals.Instance.onChangeCameraState?.Invoke(CameraStates.Initial);
+            stateDrivenCamera.Follow = null;
+            stateDrivenCamera.LookAt = null;
+            transform.position = _initalPosition;
+        }
+
+        #endregion
+    }
+}
