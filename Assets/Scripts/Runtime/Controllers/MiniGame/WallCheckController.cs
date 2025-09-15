897d05bf4a7d487a86fa3610f0d01f60d90372b5
huseyinbaglayici <huseyinow35@hotmail.com>
2025-09-16 01:28:19 +0300

    Initialized MiniGame System

---
+++ Assets/Scripts/Runtime/Controllers/MiniGame/WallCheckController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Runtime.Controllers.MiniGame
+{
+    public class WallCheckController : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Runtime/Managers/MiniGameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class MiniGameManager : MonoBehaviour
+    {
+        #region self variables
+
+        #region serialized variables
+
+        
+
+        #endregion
+
+        #endregion
+    }
+}
